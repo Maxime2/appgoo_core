2c5596b8821e3ec0793484df60e05377fa84223b
Maxim Zakharov <dp.maxime@gmail.com>
2017-01-07 22:33:05 +1100

    correct sql syntax

--- src/template_pgsql_function_end.sql
+++ src/template_pgsql_function_end.sql
@@ -60,16 +60,16 @@ exception when others then
             '<pre style="background-color:yellow; color:blue; font-size:small; line-height:normal; min-width:750px;">CONTEXT:' || replace(replace(v_error_context,'<','&lt;'),'>','&gt;') || '</pre>
             <pre style="background-color:yellow; color:blue; font-size:small; line-height:normal; min-width:750px;">SQL ERROR ' || replace(replace(sqlstate,'<','&lt;'),'>','&gt;') || ': '
             || replace(replace(sqlerrm,'<','&lt;'),'>','&gt;') || '</pre><br>' || replace(replace(array_to_string(_p_, ''),'<','&lt;'),'>','&gt;');
-    else
-        return '<pre style="position: fixed; top: 0; right: 0; bottom: 0; left: 0; background: rgba(0,0,0,0.8); z-index: 1000001; opacity:1;
-                -webkit-transition: opacity 400ms ease-in; -moz-transition: opacity 400ms ease-in; transition: opacity 400ms ease-in;
-                pointer-events: none;"></pre>
-                <pre style="width: 400px; position: relative; margin: 10% auto; padding: 5px 20px 13px 20px; border-radius: 10px;
-                background: #fff; background: -moz-linear-gradient(#fff, #999); background: -webkit-linear-gradient(#fff, #999);
-                background: -o-linear-gradient(#fff, #999); color:black; z-index: 1000002;"><h2>An unexpected error has occurred</h2>
-<p>Unfortunately, an error has occurred. This error has been recorded and will be sent to Jesla Labs. It is not known whether this error will occur repeatedly.
-If this is a serious and repeatable problem, please arrange for Jesla Labs or your partner to be aware of the impact of this issue to your business.
-You will need to refresh the page (using the Browser refresh button) to resume normal operation of Purgora.</p></pre>';
+--    else
+--        return '<pre style="position: fixed; top: 0; right: 0; bottom: 0; left: 0; background: rgba(0,0,0,0.8); z-index: 1000001; opacity:1;
+--                -webkit-transition: opacity 400ms ease-in; -moz-transition: opacity 400ms ease-in; transition: opacity 400ms ease-in;
+--                pointer-events: none;"></pre>
+--                <pre style="width: 400px; position: relative; margin: 10% auto; padding: 5px 20px 13px 20px; border-radius: 10px;
+--                background: #fff; background: -moz-linear-gradient(#fff, #999); background: -webkit-linear-gradient(#fff, #999);
+--                background: -o-linear-gradient(#fff, #999); color:black; z-index: 1000002;"><h2>An unexpected error has occurred</h2>
+--<p>Unfortunately, an error has occurred. This error has been recorded and will be sent to Jesla Labs. It is not known whether this error will occur repeatedly.
+--If this is a serious and repeatable problem, please arrange for Jesla Labs or your partner to be aware of the impact of this issue to your business.
+--You will need to refresh the page (using the Browser refresh button) to resume normal operation of Purgora.</p></pre>';
 --    end if;
 end;
 $$
